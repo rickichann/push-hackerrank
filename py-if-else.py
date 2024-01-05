ba33c9503c2c628484a008f232a8496ef7df8b57
rickichanne <rickichidayatullah@gmail.com>
2024-01-05 10:17:26 +0700

    py-if-else-solved

--- py-if-else.py
+++ py-if-else.py
@@ -5,6 +5,14 @@ import random
 import re
 import sys
 
+# Given an integer, , perform the following conditional actions:
+
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+
+# constraint  1 <= n <= 100 
 
 
 if __name__ == '__main__':
